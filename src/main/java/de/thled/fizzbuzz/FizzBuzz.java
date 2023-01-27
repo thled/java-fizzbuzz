598a843929df0fe5c9c0f249520da01ec314f72e
Thomas Le Duc <dev@tleduc.de>
2023-01-27 07:12:52 +0100

    Have a FizzBuzz play method

---
+++ src/main/java/de/thled/fizzbuzz/FizzBuzz.java
@@ -0,0 +1,8 @@
+package de.thled.fizzbuzz;
+
+public class FizzBuzz {
+
+	public String play(int number) {
+		return String.valueOf(number);
+	}
+}
--- src/test/java/de/thled/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/de/thled/fizzbuzz/FizzBuzzTest.java
@@ -1,14 +1,18 @@
 package de.thled.fizzbuzz;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
 
 	@Test
-	void works() {
-		assertTrue(true);
+	void can_call_fizzBuzz() {
+		FizzBuzz fizzbuzz = new FizzBuzz();
+
+		String result = fizzbuzz.play(0);
+
+		assertEquals("0", result);
 	}
 
 }
